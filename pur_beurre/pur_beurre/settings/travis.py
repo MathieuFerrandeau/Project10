bdb2fd66125a590fea1a291849b8466f956b3dbb
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2020-01-12 17:14:41 +0100

    tests

--- pur_beurre/pur_beurre/settings/travis.py
+++ pur_beurre/pur_beurre/settings/travis.py
@@ -1,4 +1,9 @@
 from . import *
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 
 DATABASES = {
     'default': {
